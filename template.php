ae7295311b87b1178ce80b9f09e21d0a69eb95c2
Dylan William Hardison <dylan@hardison.net>
2010-10-09 17:34:02 -0400

    remove nks stuff.

--- template.php
+++ template.php
@@ -124,7 +124,6 @@ Version 6.0 Last Updated 17 June 2010<br>
 Copyright &copy; 1997-2010 by Suncoast Linux Users Group (SLUG)<br>
 Linux is a trademark of Linus Torvalds<br>
 <!-- Open Source is a registered certification mark of the <a href="http://www.opensource.org">Open Source Initiative, Inc.</a><br> -->
-Site hosted by <a href="http://www.nks.net">Networked Knowledge Systems, Inc.</a><br>
 CSS by <a href="http://www.dream-logic.com">dreamLogic</a><br>
 </div>
 
